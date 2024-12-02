fa92b8c4d9e47262bfded9ec5d6c22f0a91f939b
julio.jimenez <jzerozumero@gmail.com>
2024-12-02 12:09:19 +0100

    Day 2, part 2 done

--- src/day_02.rs
+++ src/day_02.rs
@@ -14,11 +14,9 @@ fn part_1(input: &str) -> u64
 {
     let mut safe_lines = 0;
     let mut line_level: Level;
-    // let mut vec: Vec<u64> = Vec::new();
 
     for line in input.lines()
     {
-        // let re = Regex::new(r"^((\d+)\s+)+(\d+)$").unwrap();
         let re = Regex::new(r"(\d+)").unwrap();
 
         let matches: Vec<u64> = re.find_iter(line).map(|m| m.as_str().parse().unwrap()).collect();
@@ -77,9 +75,93 @@ fn part_1(input: &str) -> u64
     safe_lines
 }
 
-// fn part_2(input: &str) -> u64
-// {
-// }
+fn part_2(input: &str) -> u64
+{
+   let mut safe_lines = 0;
+
+    for line in input.lines()
+    {
+        let re = Regex::new(r"(\d+)").unwrap();
+
+        let matches: Vec<u64> = re.find_iter(line).map(|m| m.as_str().parse().unwrap()).collect();
+
+        assert!(matches.len() > 1);
+
+        let mut safe_line_found = false;
+
+        for i in 0..matches.len()
+        {
+            let mut v = Vec::new();
+            let line_level: Level;
+            let mut unsafe_line: bool = false;
+            for j in 0..matches.len()
+            {
+                if i != j
+                {
+                    v.push(matches[j]);
+                }
+            }
+
+            if v[0] < v[1]
+            {
+                line_level = Level::INCREASING;
+            }
+            else if v[0] > v[1]
+            {
+                line_level = Level::DECREASING;
+            }
+            else
+            {
+                line_level = Level::EQUAL;
+            }
+
+            if !unsafe_line
+            {
+                if line_level == Level::INCREASING
+                {
+                    for i in 0..(v.len()-1)
+                    {
+                        if !(v[i] < v[i+1]) ||
+                            (v[i+1] - v[i] > 3)
+                        {
+                            unsafe_line = true;
+                            break;
+                        }
+                    }
+                }
+                else if line_level == Level::DECREASING
+                {
+                    for i in 0..(v.len()-1)
+                    {
+                        if !(v[i] > v[i+1]) || 
+                            (v[i] - v[i+1] > 3)
+                        {
+                            unsafe_line = true;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    unsafe_line = true;
+                }
+
+                if !unsafe_line
+                {
+                    safe_line_found = true;
+                    break;
+                }
+            }
+        }
+
+        if safe_line_found
+        {
+            safe_lines += 1;
+        }
+    }
+    
+    safe_lines
+}
 
 pub fn main()
 {
@@ -90,8 +172,8 @@ pub fn main()
     {
         Ok(input) => 
         {
-            let result = part_1(&input);
-            // let result = part_2(&input);
+            // let result = part_1(&input);
+            let result = part_2(&input);
             println!("{result}");
         },
         Err(error) =>
@@ -115,13 +197,14 @@ mod tests {
         assert_eq!(result, 2);
     }
     
-    // #[test]
-    // fn test_2() {
-    //     let input_file = "input/day_02/test.txt";
-    //     let input = fs::read_to_string(input_file).unwrap();
+    #[test]
+    fn test_2() {
+        let input_file = "input/day_02/test.txt";
+        let input = fs::read_to_string(input_file).unwrap();
 
-    //     let result = part_2(&input);
+        let result = part_2(&input);
 
-    //     assert_eq!(result, 31);
-    // }
+        assert_eq!(result, 4);
+        // assert_eq!(result, 1);
+    }
 }
